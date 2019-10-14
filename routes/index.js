f4cc73b304dce60b5310ef4e5cbfc00490896d71
Alfiado Ngana <alfiadoprojectos@gmail.com>
2019-10-14 14:06:21 +0200

    includes

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,52 @@
 var conn = require('./../inc/db');
+var menus = require('./../inc/menus');
 var express = require('express');
 var router = express.Router();
+const title = 'Restaurante Saboroso!';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  conn.query(`
-    SELECT * FROM tb_menus ORDER BY title
-  `, (err, results)=>{
-    if(err) console.log(err);
-    else{
-      res.render('index', { title: 'Restaurante Saboroso!', menus: results });
-    }
+
+  menus.getMenus().then(results =>{
+    res.render('index', { title, menus: results });
+  });
+
+});
+
+router.get('/contacts', function(req, res, next){
+  res.render('contact', {
+    title,
+    background: 'images/img_bg_3.jpg',
+    h1: 'Diga oi!'
+  });
+});
+
+router.get('/menus', function(req, res, next){
+  menus.getMenus().then(results =>{
+    res.render('menu', {
+      title,
+      background: 'images/img_bg_1.jpg',
+      h1: 'Saboreie nosso menu!',
+      menus: results
+    });
+  });
+});
+
+  
+
+router.get('/reservations', function(req, res, next){
+  res.render('reservation', {
+    title,
+    background: 'images/img_bg_2.jpg',
+    h1: 'Reserve uma Mesa!'
+  });
+});
+
+router.get('/services', function(req, res, next){
+  res.render('services', {
+    title,
+    background: 'images/img_bg_1.jpg',
+    h1: 'É um prazer poder servir!'
   });
 });
 
